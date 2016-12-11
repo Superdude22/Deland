2e1ec96286b81eb33c0a92c55c4c0b829f891123
Mike <mpalmer22@gmail.com>
2016-12-11 13:05:08 -0500

    d

--- woocommerce.php
+++ woocommerce.php
@@ -6,9 +6,16 @@
 	
 		    <main id="main" class="small-12 columns" role="main">
 		    	
-		    	<?php  if ( ! is_product_category()||is_shop() ): ?>						
+		    	<?php  if ( !is_product_category() !is_shop() ): ?>						
 					<h1 class="hide-for-small-only"><?php the_title(); ?></h1>
-				<?php endif; ?>	 	
+				<?php endif; ?>
+
+				<?php if (is_product_category()||is_shop()){
+
+					}else{?>
+						<h1 class="hide-for-small-only"><?php the_title(); ?></h1>
+
+					<?php }	?> 	
 				
 				<?php woocommerce_content(); ?>							
 			    					
