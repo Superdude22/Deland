0b5262cc2963297084359fa967dc6f30dcfbb34e
Mike <mpalmer22@gmail.com>
2016-12-11 12:09:17 -0500

    ar

--- woocommerce.php
+++ woocommerce.php
@@ -6,7 +6,7 @@
 	
 		    <main id="main" class="small-12 columns" role="main">
 		    	
-		    	<?php  if ( ! is_shop() || is_archive() ): ?>						
+		    	<?php  if ( (!is_shop()) || (!is_archive()) ): ?>						
 					<h1 class="hide-for-small-only"><?php the_title(); ?></h1>
 				<?php endif; ?>	    	
 				
